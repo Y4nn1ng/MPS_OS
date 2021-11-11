d4af76a7ed27f0f0839d00f17cb915f0eb55e780
Y4nn1ng <77332919+Y4nn1ng@users.noreply.github.com>
2021-11-11 10:54:39 +0100

    Update Yannick.c

--- Lernaufgaben/Yannick.c
+++ Lernaufgaben/Yannick.c
@@ -1,1 +1,15 @@
+//Aufgabe 1.8
+#include <stdio.h>
 
+int days = 747244;
+
+void main() {
+    
+    printf("Jahre: %d\n", (days / 365));
+    days = days % 365;
+    printf("Wochen: %d\n", (days / 7));
+    days = days % 7;
+    printf("Tage: %d\n", days);
+    
+
+}
